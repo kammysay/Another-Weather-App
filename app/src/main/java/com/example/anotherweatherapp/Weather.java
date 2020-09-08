b85e763f7124276fc398f4b9456579fce311dc88
Samuel Kuchek <samuelkuchek@gmail.com>
2020-09-08 18:14:08 -0400

    New fields and methods.

--- app/src/main/java/com/example/anotherweatherapp/Weather.java
+++ app/src/main/java/com/example/anotherweatherapp/Weather.java
@@ -1,16 +1,20 @@
 package com.example.anotherweatherapp;
 
+
 public class Weather {
 
     static double mTemperature, mRealFeel;
-    static String mWeatherDescription, mWeatherLocation;
+    static int mID;
+    static String mWeatherDescription, mWeatherLocation = "null", mColor;
 
     //Constructor
-    public Weather(double temp, double feels_like, String description, String location){
+    public Weather(double temp, double feels_like, int weatherID, String description, String location){
         mTemperature = temp;
         mRealFeel = feels_like;
+        mID = weatherID;
         mWeatherDescription = description;
         mWeatherLocation = location;
+        mColor = setColor(weatherID);
     }
 
     public static double getTemperature() {
@@ -31,6 +35,10 @@ public class Weather {
         return String.valueOf((int)mRealFeel);
     }
 
+    public static int getWeatherID(){
+        return mID;
+    }
+
     public static String getWeatherDescription() {
         return mWeatherDescription;
     }
@@ -41,5 +49,25 @@ public class Weather {
 
     public static double kelvinToFahrenheit(double input){
         return (input - 273.15)*1.8 +32; //Kevlin to Fahrenheit Formula
+    }
+
+    public static String getColor(){
+        return mColor;
+    }
+
+    public static String setColor(int id){
+        String color;
+        if(id>=200 && id<300){ color = "thunderStorm";}
+        else if(id>=300 && id<600){ color = "drizzleOrRain";}
+        else if(id>=600 && id<700){ color = "snow";}
+        else{ color = "clearClouds";}
+        return color;
+    }
+
+    public static boolean readyToDisplay(){
+        boolean ready = false;
+        if(mWeatherLocation.equals("null")){
+            return true;
+        }else return true;
     }
 }
